97b06334d6aafc90551c6d78d4685b9d64b4fc0f
sindekai <ignacio05sebastian@gmail.com>
2024-12-16 22:18:17 -0300

    index update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const router = jsonServer.router("almacen.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 10000;
 
-server.use(cors());
+
 server.use(middlewares);
 server.use(router);
 server.listen(port);
